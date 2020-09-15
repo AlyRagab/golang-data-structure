768566710e71961bf3fb854489e2d51f54832726
Aly Ragab <alyragab70@gmail.com>
2020-09-15 04:43:56 +0200

    adding linkedlist image

---
+++ data-structure/stacks/stack.go
@@ -0,0 +1,19 @@
+// Implementing Stacks as its main options "Push" and "Pull"
+package main
+
+type Stack struct {
+	items []int
+}
+
+func (s *Stack) push(item int) {
+
+}
+
+func (s *Stack) pull() int {
+	return 0
+
+}
+
+func main() {
+
+}
